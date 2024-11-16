c8e042809972e365305754bc0940edc057a493bf
David L. Inman <davidlevere@gmail.com>
2024-11-15 23:44:58 -0500

    inc prog

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 
+#[derive(Debug)]
+pub struct Vm {
+    stack : Vec<>,
+}
+
+impl Vm {
+    pub fn new() -> Self { 
+        Vm { }
+    }
+
+    pub fn run(&mut self) -> ? {
+
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
