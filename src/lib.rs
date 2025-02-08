86e4d86f752f1f0a7c8b04a2e705c302202ca909
David L. Inman <davidlevere@gmail.com>
2025-02-07 20:02:18 -0500

    inc prog with Gen VM definition

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,36 @@
 mod data;
 mod vm;
 
+pub enum VmError {
+    X
+}
+
+pub enum P {
+    Frame(isize),
+    Base(isize),
+    Return,
+}
+
+pub enum Op<Data> {
+    Gen(Box<str>, fn(&mut Data, Vec<P>) -> Result<(), VmError>),
+    Call(usize, Vec<P>),
+    Return(P),
+}
+
+pub struct Fun<Data>(Box<str>, Vec<Op<Data>>);
+
+pub struct Vm<Data> {
+    fs : Vec<Fun<Data>>,
+    ops : Vec<Op<Data>>,
+    data : Vec<Data>,
+}
+
+impl<Data> Vm<Data> {
+    pub fn run(&mut self) -> Result<(), VmError> {
+        Err(VmError::X)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
