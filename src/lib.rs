89bb817931c2cfbe457ae67e0e9a78e6bf6e42ee
David L. Inman <davidlevere@gmail.com>
2025-02-08 00:25:18 -0500

    generic op defition

--- src/lib.rs
+++ src/lib.rs
@@ -18,23 +18,46 @@ pub enum Op {
     Return(P),
 }
 
-pub struct Fun(Box<str>, Vec<Op>);
+pub struct Fun {
+    pub name : Box<str>,
+    pub instrs : Vec<Op>,
+}
+
+pub struct GenericOp<Data> {
+    pub name : Box<str>,
+    pub op : fn(&mut Vec<Data>, &Vec<P>) -> Result<(), VmError>,
+}
 
 pub struct Vm<Data> {
     fs : Vec<Fun>,
-    ops : Vec<(Box<str>, fn(&mut Data, Vec<P>) -> Result<(), VmError>)>,
+    ops : Vec<GenericOp<Data>>,
     data : Vec<Data>,
 }
 
 struct RetAddr {
     fun : usize,
-    instr_index : usize,
+    instr : usize,
 }
 
 impl<Data> Vm<Data> {
     pub fn run(&mut self, entry : usize) -> Result<(), VmError> {
         let mut frame = self.data.len() - 1;
         let mut fun_stack : Option<RetAddr> = None;
+        let mut ip = 0;
+        let mut current = entry;
+
+        loop {
+            // TODO what if current does not exist
+            // TODO what if ip does not exist
+            match self.fs[current].instrs[ip] {
+                Op::Gen(op_index, ref params) => {
+                    // TODO what if op_index does not exist
+                    (self.ops[op_index].op)(&mut self.data, params)?;
+                    ip += 1;
+                },
+                _ => { todo!() },
+            }
+        }
         Err(VmError::X)
     }
 }
