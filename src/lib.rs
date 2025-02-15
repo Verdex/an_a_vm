fb154eb3c9c709b1d427d5ead072ce4c637be219
David L. Inman <davidlevere@gmail.com>
2025-02-15 15:16:37 -0500

    test that branch on true and don't branch on false work

--- src/lib.rs
+++ src/lib.rs
@@ -139,4 +139,64 @@ impl<Data : Clone, Unique> Vm<Data, Unique> {
 mod tests {
     use super::*;
 
+    #[test]
+    fn should_branch() {
+        const S : usize = 0;
+        const U : usize = 1;
+        const P : usize = 2;
+
+        let set_branch: GenericOp<u8, u8> = GenericOp {
+            name: "set".into(),
+            op: |_, _, _, b, _| { *b = true; Ok(()) },
+        };
+
+        let unset_branch: GenericOp<u8, u8> = GenericOp {
+            name: "unset".into(),
+            op: |_, _, _, b, _| { *b = false; Ok(()) },
+        };
+
+        let push_stack : GenericOp<u8, u8> = GenericOp {
+            name : "push".into(),
+            op: |d, _, _, _, ps | { 
+                let l = d.len() - 1;
+                if let Slot::Local(0) = ps[0] {
+                    d[l].push(0);
+                }
+                if let Slot::Local(1) = ps[0] {
+                    d[l].push(1);
+                }
+                Ok(())
+            },
+        };
+
+        let main = Fun { 
+            name: "main".into(),
+            instrs: vec![
+                Op::Gen(S, vec![]), 
+                Op::Branch(4),         
+                Op::Gen(P, vec![Slot::Local(0)]),
+                Op::ReturnSlot(Slot::Local(0)),
+
+                Op::Gen(U, vec![]),
+                Op::Branch(8),         
+                Op::Gen(P, vec![Slot::Local(1)]),
+                Op::ReturnSlot(Slot::Local(0)),
+
+                Op::Gen(P, vec![Slot::Local(0)]),
+                Op::ReturnSlot(Slot::Local(0)),
+            ],
+        };
+
+        let mut vm : Vm<u8, u8> = Vm { 
+            fs: vec![main], 
+            ops: vec![set_branch, unset_branch, push_stack], 
+            data: vec![], 
+            unique: vec![] 
+        };
+
+        let data = vm.run(0).unwrap().unwrap();
+
+        assert_eq!(data, 1);
+    }
+
 }
