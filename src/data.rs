0abeb6287015f93ad05bd094033c6a1f657a8e2c
David L. Inman <davidlevere@gmail.com>
2025-03-08 16:46:08 -0500

    move program construction data to its own file

---
+++ src/data.rs
@@ -0,0 +1,40 @@
+
+pub enum Slot { 
+    Local(usize),
+    Return,
+}
+
+pub enum Op {
+    Gen(usize, Vec<Slot>),
+    Call(usize, Vec<Slot>),
+    ReturnSlot(Slot),
+    Return,
+    Branch(usize),
+    DynCall(Vec<Slot>),
+    Yield(Slot),
+    Finish,
+    Resume(usize),
+    FinishSetBranch(usize),
+    Drop(usize),
+    Dup(usize),
+    Swap(usize, usize),
+    PushRet,
+}
+
+pub struct Fun {
+    pub name : Box<str>,
+    pub instrs : Vec<Op>,
+}
+
+pub struct OpEnv<'a, T, S> {
+    pub locals : &'a mut Vec<Vec<T>>,
+    pub globals : &'a mut Vec<S>,
+    pub ret : &'a mut Option<T>,
+    pub branch : &'a mut bool,
+    pub dyn_call : &'a mut Option<usize>,
+}
+
+pub struct GenOp<T, S> {
+    pub name : Box<str>,
+    pub op : for<'a> fn(env : OpEnv<'a, T, S>, params : &Vec<Slot>) -> Result<(), Box<dyn std::error::Error>>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,12 @@
 
 pub mod error;
+pub mod data;
 
 use crate::error::*;
+use crate::data::*;
 
 use std::borrow::Cow;
 
-pub enum Slot { 
-    Local(usize),
-    Return,
-}
-
-pub enum Op {
-    Gen(usize, Vec<Slot>),
-    Call(usize, Vec<Slot>),
-    ReturnSlot(Slot),
-    Return,
-    Branch(usize),
-    DynCall(Vec<Slot>),
-    Yield(Slot),
-    Finish,
-    Resume(usize),
-    FinishSetBranch(usize),
-    Drop(usize),
-    Dup(usize),
-    Swap(usize, usize),
-    PushRet,
-}
-
-pub struct Fun {
-    pub name : Box<str>,
-    pub instrs : Vec<Op>,
-}
-
-pub struct OpEnv<'a, T, S> {
-    pub locals : &'a mut Vec<Vec<T>>,
-    pub globals : &'a mut Vec<S>,
-    pub ret : &'a mut Option<T>,
-    pub branch : &'a mut bool,
-    pub dyn_call : &'a mut Option<usize>,
-}
-
-pub struct GenOp<T, S> {
-    pub name : Box<str>,
-    pub op : for<'a> fn(env : OpEnv<'a, T, S>, params : &Vec<Slot>) -> Result<(), Box<dyn std::error::Error>>,
-}
 
 pub struct Vm<T, S> {
     funs : Vec<Fun>,
