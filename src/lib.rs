0e3984ffbdedf8246a9d0ca56250388cbdb45935
David L. Inman <davidlevere@gmail.com>
2025-03-09 12:52:21 -0400

    memory test moved out

--- src/lib.rs
+++ src/lib.rs
@@ -435,38 +435,6 @@ mod tests {
     }
 
     #[test]
-    fn should_push_return() {
-        let push_from_global = gen_push_global();
-
-        let one = Fun { 
-            name: "one".into(),
-            instrs: vec![
-                Op::Gen(0, vec![Slot::Local(0)]), 
-                Op::ReturnSlot(Slot::Local(0)),
-            ],
-        };
-
-        let main = Fun { 
-            name: "main".into(),
-            instrs: vec![
-                Op::Call(1, vec![]),
-                Op::PushRet,
-                Op::ReturnSlot(Slot::Local(0)),
-            ],
-        };
-
-        let mut vm : Vm<u8, u8> = Vm::new(
-            vec![main, one], 
-            vec![push_from_global]);
-
-        vm.with_globals(vec![3]);
-
-        let data = vm.run(0).unwrap().unwrap();
-
-        assert_eq!(data, 3);
-    }
-    
-    #[test]
     fn should_dynamic_call() {
         const ONE : usize = 0;
         const TWO : usize = 1;
