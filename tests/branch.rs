8d568b99dc430833e874882c5ca40c7fe6234e91
David L. Inman <davidlevere@gmail.com>
2025-03-12 21:29:22 -0400

    coroutine finished set branch test for in resumed coroutine

--- tests/branch.rs
+++ tests/branch.rs
@@ -306,5 +306,53 @@ fn should_branch_on_finished_coroutine_in_dyn_function_where_parent_has_active_c
 
     assert_eq!(data, 5);
 }
-// TODO branch on finnished coroutine inside of function while return function has active coroutine in same slot
-// TODO same as above but for already resumed coroutine 
+
+#[test]
+fn should_branch_on_finished_coroutine_in_resumed_coroutine_where_parent_has_active_coroutine() {
+    let push_from_global = common::gen_push_global();
+
+    let co = Fun {
+        name: "co".into(),
+        instrs: vec![
+            Op::Gen(0, vec![Slot::Local(0)]),
+            Op::Yield(Slot::Local(0)),
+            Op::Finish,
+        ],
+    };
+
+    let child = Fun {
+        name: "child".into(),
+        instrs: vec![
+            Op::Call(2, vec![]),
+            Op::Gen(0, vec![Slot::Local(0)]),
+            Op::Yield(Slot::Local(0)),
+            Op::Resume(0),
+            Op::FinishSetBranch(0),
+            Op::Branch(7),
+            Op::Gen(0, vec![Slot::Local(1)]),
+            Op::Gen(0, vec![Slot::Local(2)]),
+            Op::Yield(Slot::Local(1)),
+            Op::Finish,
+        ],
+    };
+
+    let main = Fun { 
+        name: "main".into(),
+        instrs: vec![
+            Op::Call(2, vec![]),
+            Op::Call(1, vec![]),
+            Op::Resume(1),
+            Op::ReturnSlot(Slot::Return),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new(
+        vec![main, child, co], 
+        vec![push_from_global]);
+
+    vm.with_globals(vec![1, 3, 5]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 5);
+}
