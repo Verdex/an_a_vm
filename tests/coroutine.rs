7610e0564fff94160a3b0f53825776ed65b5ed9d
David L. Inman <davidlevere@gmail.com>
2025-03-12 23:22:49 -0400

    todo tests

--- tests/coroutine.rs
+++ tests/coroutine.rs
@@ -3,3 +3,16 @@ pub mod common;
 
 use an_a_vm::*;
 use an_a_vm::data::*;
+
+
+// TODO 
+// should yield
+// should resume
+// should handle params
+// call
+// dyn call
+// multiple coroutines interleaved inside of a coroutine
+// finish set branch kills off finished coroutine
+// finish set branch doesnt kill off active coroutine
+// resuming coroutine pulls coroutine out of order
+// yielding or finishing coroutine puts it in the end of the coroutine list
