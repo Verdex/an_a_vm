6d58217ef6f23e807c1c545390331e1c2166ed43
David L. Inman <davidlevere@gmail.com>
2025-03-14 20:33:03 -0400

    handle params

--- tests/coroutine.rs
+++ tests/coroutine.rs
@@ -78,10 +78,54 @@ fn should_resume() {
     assert_eq!(data, 12);
 }
 
+#[test]
+fn should_handle_params() {
+    let push_from_global = common::gen_push_global();
+    let add = common::gen_add();
+    let mul = common::gen_mul();
+
+    let co = Fun {
+        name: "co".into(),
+        instrs: vec![
+            Op::Gen(1, vec![0, 1]),
+            Op::PushRet,
+            Op::Yield(Slot::Local(3)),
+            Op::Gen(2, vec![3, 2]),
+            Op::Yield(Slot::Return),
+            Op::Finish,
+        ],
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![0]),
+            Op::Gen(0, vec![1]),
+            Op::Gen(0, vec![2]),
+            Op::Call(1, vec![Slot::Local(0), Slot::Local(1), Slot::Local(2)]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet,
+            Op::Resume(0),
+            Op::ReturnSlot(Slot::Return),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main, co],
+        vec![push_from_global, add, mul]);
+
+    vm.with_globals(vec![3, 5, 7]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 56);
+}
+
 // TODO 
 // should handle params
-// call
-// dyn call
+// dyn call (and params)
 // multiple coroutines interleaved inside of a coroutine
 // finish set branch kills off finished coroutine
 // finish set branch doesnt kill off active coroutine
