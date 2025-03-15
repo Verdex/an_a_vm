77133921b37750a56fe7fa463f79b7168ccb7430
David L. Inman <davidlevere@gmail.com>
2025-03-15 17:49:59 -0400

    interleaved coroutine test

--- tests/coroutine.rs
+++ tests/coroutine.rs
@@ -359,5 +359,149 @@ fn should_move_coroutine_position_on_resume_yield() {
     assert_eq!(data, 1);
 }
 
-// TODO 
-// multiple coroutines interleaved inside of a coroutine
+#[test]
+fn should_handle_coroutine_with_interleaved_coroutines() {
+    let push_from_global = common::gen_push_global();
+    let set_branch = common::gen_set_branch();
+    let add = common::gen_add();
+
+    let blarg = GenOp {
+        name: "balfg".into(),
+        op: | env, params |  { 
+            if let [s] = &params[..] {
+                let a = &env.locals.last().unwrap()[*s];
+                println!("{:?}", a);
+            }
+            Ok(())
+        },
+    };
+
+    let inf = Fun {
+        name: "inf".into(),
+        instrs: vec![
+            Op::Yield(0),
+            Op::Gen(1, vec![]),
+            Op::Branch(0),
+        ],
+    };
+
+    let com = Fun {
+        name: "com".into(),
+        instrs: vec![
+            Op::Gen(0, vec![0]),
+            Op::Gen(0, vec![1]),
+            Op::Gen(0, vec![2]),
+            Op::Call(2, vec![0]),
+            Op::PushRet,
+            Op::Yield(3),
+            Op::Call(2, vec![1]),
+            Op::PushRet,
+            Op::Gen(2, vec![3, 4]),
+            Op::PushRet,
+            Op::Yield(5),
+            Op::Call(2, vec![2]),
+            Op::PushRet,
+            Op::Gen(2, vec![5, 6]),
+            Op::PushRet,
+            Op::Yield(7),
+            Op::Drop(6),
+            Op::Drop(5),
+            Op::Drop(4),
+            Op::Drop(3),
+            Op::Drop(2),
+            Op::Drop(1),
+            Op::Drop(0),
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet,
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet,
+            Op::Yield(0),
+            Op::Gen(1, vec![]),
+            Op::Branch(23),
+            Op::Finish,
+        ],
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Call(1, vec![]),
+            Op::PushRet,
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 1 + 3
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 4 + 6
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0), 
+            Op::PushRet, // 10 + 9
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 19 + 13
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 32 + 18
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 50 + 21
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 71 + 25
+
+            Op::Resume(0),
+            Op::PushRet,
+            Op::Gen(2, vec![0, 1]),
+            Op::Drop(0),
+            Op::Drop(0),
+            Op::PushRet, // 96 + 20
+
+            Op::ReturnLocal(0), // 126
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main, com, inf],
+        vec![push_from_global, set_branch, add, blarg]);
+
+    vm.with_globals(vec![1, 2, 3]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 126);
+}
