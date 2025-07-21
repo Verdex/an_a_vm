9bc83452b8a6e01f4f8387a3ec220fc2c3455970
David L. Inman <davidlevere@gmail.com>
2025-07-20 20:07:16 -0400

    should not return local

--- tests/gen_op.rs
+++ tests/gen_op.rs
@@ -3,12 +3,40 @@ pub mod common;
 
 use an_a_vm::*;
 use an_a_vm::data::*;
+use an_a_vm::error::*;
+
+#[test]
+fn should_not_return_from_local() {
+    let op = GenOp::Local {
+        name: "op".into(),
+        op: | _locals, _params |  { 
+            Ok(None)
+        },
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![]),
+            Op::PushRet,
+            Op::ReturnLocal(0),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main],
+        vec![op]);
+
+    let error = vm.run(0);
+
+    assert!(matches!(error, Err(VmError::AccessMissingReturn(_))));
+}
 
 #[test]
 fn should_return_from_local() {
     let op = GenOp::Local {
         name: "op".into(),
-        op: | locals, params |  { 
+        op: | _locals, _params |  { 
             Ok(Some(3))
         },
     };
