75f2313f89fa51c9b1179fcab0be43dd34eeda9d
David L. Inman <davidlevere@gmail.com>
2025-07-22 22:00:21 -0400

    global tests

--- tests/gen_op.rs
+++ tests/gen_op.rs
@@ -6,6 +6,135 @@ use an_a_vm::data::*;
 use an_a_vm::error::*;
 
 #[test]
+fn should_modify_global() {
+    let op = GenOp::Global {
+        name: "op".into(),
+        op: | globals, params |  { 
+            globals[params[0]] += 1;
+            Ok(None)
+        },
+    };
+
+    let ret_glob = GenOp::Global {
+        name: "ret_glob".into(),
+        op: | globals, _params | {
+            Ok(Some(globals[1]))
+        },
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![1]),
+            Op::Gen(1, vec![]),
+            Op::PushRet,
+            Op::ReturnLocal(0),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main],
+        vec![op, ret_glob]);
+
+    vm.with_globals(vec![0, 3]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 4);
+}
+
+#[test]
+fn should_push_global() {
+
+    let op = GenOp::Global {
+        name: "op".into(),
+        op: | globals, _params |  { 
+            globals.push(3);
+            Ok(None)
+        },
+    };
+
+    let ret_glob = GenOp::Global {
+        name: "ret_glob".into(),
+        op: | globals, _params | {
+            Ok(Some(globals[0]))
+        },
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![]),
+            Op::Gen(1, vec![]),
+            Op::PushRet,
+            Op::ReturnLocal(0),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main],
+        vec![op, ret_glob]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 3);
+}
+
+#[test]
+fn should_not_return_from_global() {
+    let op = GenOp::Global {
+        name: "op".into(),
+        op: | _globals, _params |  { 
+            Ok(None)
+        },
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![]),
+            Op::PushRet,
+            Op::ReturnLocal(0),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main],
+        vec![op]);
+
+    let error = vm.run(0);
+
+    assert!(matches!(error, Err(VmError::AccessMissingReturn(_))));
+}
+
+#[test]
+fn should_return_from_global() {
+    let op = GenOp::Global {
+        name: "op".into(),
+        op: | _globals, _params |  { 
+            Ok(Some(3))
+        },
+    };
+
+    let main = Fun {
+        name: "main".into(),
+        instrs: vec![
+            Op::Gen(0, vec![]),
+            Op::PushRet,
+            Op::ReturnLocal(0),
+        ],
+    };
+
+    let mut vm : Vm<usize, usize> = Vm::new( 
+        vec![main],
+        vec![op]);
+
+    let data = vm.run(0).unwrap().unwrap();
+
+    assert_eq!(data, 3);
+}
+
+#[test]
 fn should_modify_local() {
     let op = GenOp::Local {
         name: "op".into(),
